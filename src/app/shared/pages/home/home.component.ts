78d692ccb3bcec4d5ef30531be07a52ec00ceb01
srimaharshi manchem <email2rishi13@gmail.com>
2019-02-26 08:36:36 +0530

    daily update

--- src/app/shared/pages/home/home.component.ts
+++ src/app/shared/pages/home/home.component.ts
@@ -5,27 +5,27 @@ import { trigger, state, style, transition, animate } from '@angular/animations'
   selector: 'dig-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.less'],
-  animations: [
-    trigger('openClose', [
-      // ...
-      state('open', style({
-        height: '200px',
-        opacity: 1,
-        backgroundColor: 'yellow'
-      })),
-      state('closed', style({
-        height: '100px',
-        opacity: 0.5,
-        backgroundColor: 'green'
-      })),
-      transition('open => closed', [
-        animate('1s')
-      ]),
-      transition('closed => open', [
-        animate('0.5s')
-      ]),
-    ]),
-  ],
+  // animations: [
+  //   trigger('openClose', [
+  //     // ...
+  //     state('open', style({
+  //       height: '200px',
+  //       opacity: 1,
+  //       backgroundColor: 'yellow'
+  //     })),
+  //     state('closed', style({
+  //       height: '100px',
+  //       opacity: 0.5,
+  //       backgroundColor: 'green'
+  //     })),
+  //     transition('open => closed', [
+  //       animate('1s')
+  //     ]),
+  //     transition('closed => open', [
+  //       animate('0.5s')
+  //     ]),
+  //   ]),
+  // ],
 })
 export class HomeComponent implements OnInit {
 
